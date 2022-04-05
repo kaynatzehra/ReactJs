6b53bfb753f35e2d103169ea6893b186ef77ff24
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-04-05 23:12:58 +0530

    Added:  User Input name (onChange)

--- first_projct/src/App.js
+++ first_projct/src/App.js
@@ -8,20 +8,33 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      title: 'Welcome, click the logo' 
+      userName:''
     }
   }
+
   onClick=($event) =>{
     console.log(" save button is clicked:", $event );
     window.open(this.url, "_blank")
   }
+
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value );
+    this.setState ({ userName: event.target.value})
+
+  }
+
   render(){
     return (
+      <>
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>Hello {this.state.userName}, from BridgeLabz</h1>
         <img src={logo} onClick={this.onClick}
         alt="The Bridglabz logo : a Bridge to Employment through  lab works " />
       </div>
+      <div>
+        <input onChange={this.onNameChange} />
+      </div>
+      </>
     );
   }
 }
