8f96e14a7e8473081a69176e154ce2279592a2e0
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-04-05 23:49:05 +0530

    Added :  Ensuring User Input is Valid

--- first_projct/src/App.js
+++ first_projct/src/App.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import logo from './BridgelabzLOGO.PNG';
-import './App.css';
 
 
 class App extends React.Component{
-  url= "https://www.bridgelabz.com/"
-  constructor(){
-    super()
-    this.state = {
-      userName:''
+    url="https:www.bridgelabz.com/"
+    constructor(){
+        super()
+        this.state = {
+            title : 'Hello ,from BridgeLabz',
+            username : '',
+            error:''
+        }
     }
-  }
-
-  onClick=($event) =>{
-    console.log(" save button is clicked:", $event );
-    window.open(this.url, "_blank")
-  }
-
-  onNameChange = (event) => {
-    console.log("value is ", event.target.value );
-    this.setState ({ userName: event.target.value})
-
-  }
-
-  render(){
-    return (
-      <>
-      <div>
-        <h1>Hello {this.state.userName}, from BridgeLabz</h1>
-        <img src={logo} onClick={this.onClick}
-        alt="The Bridglabz logo : a Bridge to Employment through  lab works " />
-      </div>
-      <div>
-        <input onChange={this.onNameChange} />
-      </div>
-      </>
-    );
-  }
+    imageclick = () =>{
+            window.open(this.url,'_blank');
+    }
+    namechange = (event) =>{
+        this.setState({username : event.target.value})
+        const nameregex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if(nameregex.test(event.target.value)){
+            this.setState({error : ''})
+        }
+        else{
+            this.setState({error : 'Naming Error'})
+        }
+    }
+    render(){
+        return(
+            <div>
+                
+                <h1 style={{"color":"Blue"}}>Hello {this.state.username} from bridgelabz</h1>
+                <img src={logo} className="logo" alt='BridgLabz' onClick={this.imageclick}/><br/>
+                <input type="text" placeholder='Your Name Here' onChange={this.namechange}/><br/><br/>
+                <span className='error'>{this.state.error}</span>
+            </div>
+        );
+    }
 }
 export default App;
