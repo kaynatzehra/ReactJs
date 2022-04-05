7c3b0a0702ab11a79ebed50da2d0ee6d85d51b5f
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-04-05 23:06:46 +0530

    Added: Logo by Property binding

--- first_projct/src/App.js
+++ first_projct/src/App.js
@@ -1,17 +1,19 @@
+import logo from './BridgelabzLOGO.PNG';
 import './App.css';
 
 function App() {
   var name = 'Kaynat'
 
-  
 
   
 
   return (
     <div>
-    {/* <p>Hello from BridgeLabz </p> */}
-    <h1> Hello {name}, from Bridgelabz</h1>
+    <p>Hello from BridgeLabz</p>
+    <h1>Hello {name}, from Bridgelabz</h1>
 
+    
+    <img src = {logo} alt ="Just Logo" />
     </div>
   );
 }
